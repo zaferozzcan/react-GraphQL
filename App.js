edec5edf6655d969b6dc51b1f3fc764638973181
Ecem Bademli <ecembademli@Ecems-MacBook-Pro.local>
2021-03-09 22:26:32 -0500

    add style on the main page

--- App.js
+++ App.js
@@ -1,32 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import CarsList from './components/CarsList';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <View style={styles.carContainer}>
-        <View style={style.title}>
-          <text>
-            Model S
-          </text>
-          <text>Starting at $69,400</text>
-        </View>
-      
+    <View style={styles.container}>
+      {/* <Header />
+      <CarsList />
+      <StatusBar style="auto" /> */}
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    fontSize:"100px",
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  carContainer:{
-
-  }
-  
 });
