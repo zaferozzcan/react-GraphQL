365c4d5698b0a5a28923e71dc2d2fb4a0d85d606
Ecem Bademli <ecembademli@Ecems-MacBook-Pro.local>
2021-03-09 21:56:00 -0500

    add some title on the app

--- App.js
+++ App.js
@@ -4,18 +4,29 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.carContainer}>
+        <View style={style.title}>
+          <text>
+            Model S
+          </text>
+          <text>Starting at $69,400</text>
+        </View>
+      
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    fontSize:"100px",
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  carContainer:{
+
+  }
+  
 });
